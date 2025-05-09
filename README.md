c4d906544438f9adf52b3793bff9a3b705629b32
FLD-TN <duyhsne@gmail.com>
2025-05-09 23:06:35 +0700

    dos: update file README.md

---
+++ README.md
@@ -0,0 +1,2 @@
+Xin Chào tới project môn Lập Trình Java
+- Tên đề tài : Ứng dụng quản lí bán trà sữa 
---
+++ src/main/java/com/fldtn/doan/milkteashopmanage/Test.java
@@ -0,0 +1,7 @@
+package com.fldtn.doan.milkteashopmanage;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
