03ff4f7b65ac387f3fcc4af37ec35afb542d624c
FLD-TN <duyhsne@gmail.com>
2025-05-10 22:17:34 +0700

    docs: update trang chủ

--- src/main/java/com/fldtn/doan/milkteashopmanage/Main.java
+++ src/main/java/com/fldtn/doan/milkteashopmanage/Main.java
@@ -6,6 +6,29 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main  {
+import javax.swing.*;
+import javafx.scene.image.Image;
 
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Home/Home.fxml"));
+        Parent root = loader.load();
+        Scene scene = new Scene(root);
+
+        Image icon = new Image(getClass().getResourceAsStream("/images/milktea_icon.png"));
+        primaryStage.getIcons().add(icon);
+
+        primaryStage.setTitle("Quản Lí Trà Sữa Futuree");
+        scene.getStylesheets().add(getClass().getResource("/fxml/Home/style.css").toExternalForm());
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
