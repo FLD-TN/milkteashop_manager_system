2d7b1a44638614ea6ad3dff2f431c6918818381a
FLD-TN <duyhsne@gmail.com>
2025-05-10 22:59:24 +0700

    docs:update trang chủ có thể điều chỉnh sidebar

--- src/main/java/com/fldtn/doan/milkteashopmanage/Main.java
+++ src/main/java/com/fldtn/doan/milkteashopmanage/Main.java
@@ -2,13 +2,14 @@ package com.fldtn.doan.milkteashopmanage;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import javax.swing.*;
-import javafx.scene.image.Image;
-
 
 public class Main extends Application {
     @Override
@@ -22,10 +23,24 @@ public class Main extends Application {
 
         primaryStage.setTitle("Quản Lí Trà Sữa Futuree");
         scene.getStylesheets().add(getClass().getResource("/fxml/Home/style.css").toExternalForm());
+
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+
+        primaryStage.setWidth(1024);
+        primaryStage.setHeight(768);
+
+        primaryStage.setX(bounds.getMinX() + (bounds.getWidth() - 1024) / 2);
+        primaryStage.setY(bounds.getMinY() + (bounds.getHeight() - 768) / 2);
+
+        primaryStage.setResizable(true);
+        primaryStage.setMinWidth(1024);
+        primaryStage.setMinHeight(768);
+
+        primaryStage.setMaximized(true);
+
         primaryStage.setScene(scene);
         primaryStage.show();
-
-        
     }
 
     public static void main(String[] args) {
